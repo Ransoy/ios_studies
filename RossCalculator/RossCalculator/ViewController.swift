0d317b886385a82cc3626a85cb38839d9f83ea2e
Ransoy <nr.rances@gmail.com>
2018-03-15 11:39:54 +0800

    my studies calculator

---
+++ RossCalculator/RossCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RossCalculator
+//
+//  Created by FDC Yongbo on 15/03/2018.
+//  Copyright © 2018 FDC Yongbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
